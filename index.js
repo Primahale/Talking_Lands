f1f2df2af332b2e6276551c80f8b39883806125a
Priyanka Mahale <97458322+Primahale@users.noreply.github.com>
2024-11-16 19:19:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Polygon = require('./models/Polygon');
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: 'https://talking-lands.onrender.com'}));
 
 const URL = process.env.MONGO_URL;
 console.log('MongoDB URI:', URL);
